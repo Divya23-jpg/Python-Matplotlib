3236f982a671f387ee072603ee31368fe90134b7
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-23 01:00:24 +0530

    divya

---
+++ ch 5 Plotting Bar Charts,Pie charts , Histogram/histogram.py
@@ -0,0 +1,13 @@
+"""
+? Definition:  
+A histogram is a type of chart that represents the distribution of numerical data.
+1.Data is grouped into intervals (bins).
+2.Each bar shows the frequency (count) of values falling within that interval.
+3.Unlike bar charts, histograms deal with continuous data, not categories.
+
+? Why Analysts Use Histograms
+1.Understand distribution: Shows how data is spread (normal, skewed, uniform, etc.).
+2.Detect patterns: Identify central tendency, spread, and shape of data.
+3.Spot outliers: Quickly see unusual values outside the main distribution.
+4.Support statistical analysis: Useful in probability, regression, and quality control.
+"""
