dc79efa5a3ab9a5dc1cec5e0d0f44fdf2138f89f
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-22 12:52:12 +0530

    divya

---
+++ ch 2 Introduction to Matplotlib/matplotlib_intro.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+
+# ? matplotlib: Main Package
+# ! .pylpot: In Built  Modules  in python to provide functionality to creat plot
+#  plt: Aliase or named of this matplotlib.pyplot  so that we use as a short
