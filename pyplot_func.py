20aef700992710f8fc23ae6118b33ac611e97a00
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-22 19:36:54 +0530

    divya

--- pyplot_func.py
+++ pyplot_func.py
@@ -1,2 +1,20 @@
+import matplotlib.pyplot as plt
 
-# !
+# 1. plt.plot(): Used for line graph
+# ! plt.plot(x, y, colour='color name', linestyle='Line_style', linewidth=value, marker='marker symbol',label='label name')
+
+
+
+months=[1,2,3,4,5]
+sales=[1000,1500,2000,4000,1200]
+
+plt.plot(months,sales,color='blue',linestyle='--', linewidth=2,marker='o',label='2025 Sales Data')
+
+# 2.plt.xlabel(): used for label on a X-axis 
+# ! plt.xlabel('text)
+# 3.plt.ylabel(): used for label on a Y-axis 
+# ! plt.ylabel('text)
+
+plt.xlabel('Number of Months')
+plt.ylabel('Number of Sales per month')
+plt.show()
