a0705079c7addc363cb2bacfa90b85350981e7ee
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-22 17:50:53 +0530

    divya

---
+++ ch 4 Understanding Pyplot/pyplot.py
@@ -0,0 +1,16 @@
+"""Pyplot in Matplotlib is a module that provides a simple, MATLAB-like
+interface for creating plots and visualizations. 
+It acts as a collection of functions that let you build and customize figures step by step, making plotting in Python straightforward and intuitive
+"""
+
+import matplotlib.pyplot as plt
+
+x=['Mon','Tue','Wed','Thu','Fri'] # X- Axis
+y=[10,15,7,20,12] # Y-Axis
+plt.plot(x,y)
+plt.title("Bakery Sales This Week")
+plt.xlabel("Day of the Week")
+plt.ylabel("Number of Cake Sale")
+plt.show()
+
+
