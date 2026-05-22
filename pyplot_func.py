5b27e36de967c5225aeaf2f37b07c2133a3907e8
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-23 00:48:35 +0530

    divya

--- pyplot_func.py
+++ pyplot_func.py
@@ -21,4 +21,29 @@ plt.ylabel('Number of Sales per month')
 # 4 plt.title() : used to show heading of the graph
 # ! plt.title('text')
 plt.title('Monthly Sales Data Report')
+
+
+# 5 plt.legend(): used to show small box and tell which marker and line presents
+# ! plt.legend(loc='location,font size=value): Parameters are fully optional
+# ? loc='upper left'  loc='lower left'
+# ? Legend only see when we add Label
+plt.legend(loc='upper left',fontsize=12)
+
+# 6 plt.grid(): used to show boxes in the area of graph
+# ! plt.grid(color='colur name',linestyle='style',linewidth=value):
+plt.grid(color='grey',linestyle=':',linewidth=1)
+
+
+#  7 plt.xlim(str,end): used to limit X Axis
+#  7 plt.ylim(str,end): used to limit y Axis
+# ! plt.xlim(start,end)  and  plt.ylim(start,end)
+
+plt.xlim(1,4)
+plt.ylim(0,2000)
+
+# ! plt.xticks(old,new): used to replace labeling  x points 
+# ! plt.yticks(old,new): used to replace labeling y points 
+plt.xticks([1,2,3,4,5], ['M1','M2','M3','M4','M5'])
+
+# plt.show(): T0 display a plot on the screen
 plt.show()
