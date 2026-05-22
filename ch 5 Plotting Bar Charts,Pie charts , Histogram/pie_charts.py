207684e83369845445962db7d1a9a1aed65b66ae
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-23 00:58:43 +0530

    divya

---
+++ ch 5 Plotting Bar Charts,Pie charts , Histogram/pie_charts.py
@@ -0,0 +1,14 @@
+"""
+? Definition:  
+A pie chart is a circular statistical graphic divided into slices to illustrate numerical proportions.
+1.Each slice represents a category’s contribution to the whole.
+2.The size of the slice is proportional to its percentage of the total.
+
+
+? Why Analysts Use Pie Charts
+1.Show proportions: Best for displaying how a dataset is split into parts.
+2.Quick visualization: Easy to understand at a glance.
+3.Highlight dominance: Makes it clear which category has the largest share.
+4.Percentage focus: Useful when the total is 100% and categories are mutually exclusive.
+
+"""
