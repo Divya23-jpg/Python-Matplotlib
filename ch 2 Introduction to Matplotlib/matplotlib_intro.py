5998b0b0b73f8affbd5fdb7ac37701cf74621d8c
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-22 13:15:03 +0530

    divya

--- ch 2 Introduction to Matplotlib/matplotlib_intro.py
+++ ch 2 Introduction to Matplotlib/matplotlib_intro.py
@@ -5,3 +5,7 @@ import matplotlib.pyplot as plt
 #  plt: Aliase or named of this matplotlib.pyplot  so that we use as a short
 
 
+x=[1,2,3,4]  # horizontal axis
+y=[10,20,15,25]   # Vertical axis
+plt.plot(x,y)
+plt.show()
