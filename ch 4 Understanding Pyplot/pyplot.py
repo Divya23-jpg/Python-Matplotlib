ef084f79bffb896c043f039151c0b767e93db059
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-22 18:23:13 +0530

    divya

--- ch 4 Understanding Pyplot/pyplot.py
+++ ch 4 Understanding Pyplot/pyplot.py
@@ -2,9 +2,10 @@
 interface for creating plots and visualizations. 
 It acts as a collection of functions that let you build and customize figures step by step, making plotting in Python straightforward and intuitive
 """
-
+# ? Most Imp: Without data ploting cannot possible
 # ! Line Grapg for Cakes Sales per Week Day 
 import matplotlib.pyplot as plt
+
 
 x=['Mon','Tue','Wed','Thu','Fri'] # X- Axis
 y=[10,15,7,20,12] # Y-Axis
