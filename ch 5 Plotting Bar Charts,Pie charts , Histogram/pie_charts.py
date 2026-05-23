b1a53237a424175b6480850bd3744b7ee749fbdd
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-23 19:21:31 +0530

    divya

--- ch 5 Plotting Bar Charts,Pie charts , Histogram/pie_charts.py
+++ ch 5 Plotting Bar Charts,Pie charts , Histogram/pie_charts.py
@@ -22,4 +22,5 @@ regions=['North','South','East','West']
 revenue=[3000,2000,1500,1000]
 plt.pie(revenue,labels=regions,autopct='%1.1f%%',colors=['gold','skyblue','lightgreen','coral'])
 plt.title("Revenue Contribution By Region")
+
 plt.show()
