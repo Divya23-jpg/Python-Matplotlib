2b085a792cdf3cc5a2a1887825b012e95f723e9e
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-24 18:43:18 +0530

    divya

--- ch 6 Scatter plot/scatter_plot.py
+++ ch 6 Scatter plot/scatter_plot.py
@@ -13,12 +13,26 @@ import matplotlib.pyplot as plt
 # ? plt.scatter(x,y,color='color_name',marker='marker style',label='label name')
 # marker means dot type or  which type of point you want to show
 
-hours_stu=[1,2,3,4,5,6,7,8]
-exam_scors=[50,55,60,65,70,75,80,85]
-plt.scatter(hours_stu,exam_scors,color='Red',marker='o',label='Student Data')
+# hours_stu=[1,2,3,4,5,6,7,8]
+# exam_scors=[50,55,60,65,70,75,80,85]
+# plt.scatter(hours_stu,exam_scors,color='Red',marker='D',label='Student Data')
+# plt.xlabel("Study Hours Of a Student")
+# plt.ylabel("Exam Scores Of a Student")
+# plt.title("Relation Between Study Time and Exam Scores")
+# plt.legend()
+# plt.grid('True')
+# plt.show()
+
+
+
+# ! For Comparison of Two Data set
+# Class A
+plt.scatter([1,2,3],[50,55,70],color='blue',label='Class A')
+# Class B
+plt.scatter([1,2,3],[45,52,50],color='yellow',label='Class A')
 plt.xlabel("Study Hours Of a Student")
 plt.ylabel("Exam Scores Of a Student")
-plt.title("Relation Between Study Time and Exam Scores")
+plt.title("Comparision Of Two Classes")
 plt.legend()
 plt.grid('True')
 plt.show()
