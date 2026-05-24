27af8b67a069c5316488a9dc4f7c80cd3c2536bf
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-24 22:10:05 +0530

    divya

--- ch 9 Project By Netfix Dataset/netfix.py
+++ ch 9 Project By Netfix Dataset/netfix.py
@@ -5,6 +5,8 @@ import pandas as pd
 
 
 # ! Step 2 : Load the data
-from pathlib import Path
-file_path = Path("C:/Users/Divya/Desktop/netflix_dataset.csv")
-df = pd.read_csv(file_path)
+# from pathlib import Path
+# file_path = Path("netflix_dataset.csv")
+# df = pd.read_csv(file_path)
+
+df=pd.read_csv("netflix_dataset.csv")
