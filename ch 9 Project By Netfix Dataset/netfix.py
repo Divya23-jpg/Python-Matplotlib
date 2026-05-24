4a8414d89c464f9bd0411445f285b13847f35767
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-25 01:35:03 +0530

    divya

--- ch 9 Project By Netfix Dataset/netfix.py
+++ ch 9 Project By Netfix Dataset/netfix.py
@@ -14,30 +14,50 @@ df=pd.read_csv("netflix_titles.csv")
 
 df=df.dropna(subset=['type','release_year','rating','country','duration'])
 type_counts= df['type'].value_counts()
-print(df[['type','release_year','rating','country','duration']].isnull().sum())
+
+# ! For Showing Relationship Between Movies Vs Tv shows (Bar Graph)
 plt.figure(figsize=(6,4))
 plt.bar(type_counts.index,type_counts.values,color=['skyblue','orange'])
 plt.title("Number of Movies VS TV shows on Netfix")
 plt.xlabel("Type")
 plt.ylabel("Count")
 plt.tight_layout()
-
 plt.savefig('movies_vs_tyshows.png')
+# plt.show()
+
+# ! For Showing Rating in Percentage (Pie chart)
+rating_counts=df['rating'].value_counts()
+plt.figure(figsize=(8,6))
+plt.pie(rating_counts,labels=rating_counts.index,autopct='%1.1f%%',startangle=90)
+plt.title("Percentage  of rating")
+plt.tight_layout()
+plt.savefig('content_rating_pie.png')
+# plt.show()
+
+
+# ! For showing Movies Duration according to Minutes (Histogram)
+movies_df=df[df['type']=="Movie"].copy()
+movies_df['duration_int']=movies_df['duration'].str.replace('min','').astype(int)
+
+plt.figure(figsize=(8,6))
+plt.hist(movies_df['duration_int'],bins=30,color='purple',edgecolor='black')
+plt.title('Distribution Of Movie Duration')
+plt.xlabel("Duration Minute")
+plt.ylabel("Number of Movies")
+plt.tight_layout()
+plt.savefig('movies_duration_histo.png')
 plt.show()
 
 
-# import matplotlib.pyplot as plt
 
-# # Only drop rows where 'type' is missing
-# df = df.dropna(subset=['type'])
-# type_counts = df['type'].value_counts()
+# movies_df = df[df['type'] == "Movie"].copy()
+# movies_df['duration_int'] = movies_df['duration'].str.replace('min', '').astype(int)
 
-# plt.figure(figsize=(6,4))
-# plt.bar(type_counts.index, type_counts.values, color=['skyblue','orange'])
-# plt.title("Number of Movies vs TV Shows on Netflix")
-# plt.xlabel("Type")
-# plt.ylabel("Count")
+# plt.figure(figsize=(8,6))
+# plt.hist(movies_df['duration_int'], bins=30, color='purple', edgecolor='black')
+# plt.title('Distribution Of Movie Duration')
+# plt.xlabel("Duration Minute")
+# plt.ylabel("Number of Movies")
 # plt.tight_layout()
-
-# plt.savefig('movies_vs_tvshows.png')
+# plt.savefig('movies_duration_histo.png')
 # plt.show()
