cf736e51a651cc5bcb9621e34ea11486d08a2065
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-24 18:20:33 +0530

    divya

--- ch 5 Plotting Bar Charts,Pie charts , Histogram/histogram.py
+++ ch 5 Plotting Bar Charts,Pie charts , Histogram/histogram.py
@@ -13,10 +13,15 @@ A histogram is a type of chart that represents the distribution of numerical dat
 3.Spot outliers: Quickly see unusual values outside the main distribution.
 4.Support statistical analysis: Useful in probability, regression, and quality control.
 
-
+# ? use when numerical continious data we have
 """
 import matplotlib.pyplot as plt
+# bins : ranges or frequency dividedd python automatically when we give no of range
+# ! plt.hist(data,bins=[num of bins],color='color name',edgecolor='colour')
 
-# ! 
-x=['Mon','Tue','Wed','Thu']
-
+scores=[45,67,89,56,78,88,92,60,74,81,59,66,75,82,90,85,70,73,68,77]
+plt.hist(scores,bins=5,color='purple',edgecolor='black')
+plt.xlabel("Scores Range")
+plt.xlabel("Number of students")
+plt.title("Score Distribution")
+plt.show()
