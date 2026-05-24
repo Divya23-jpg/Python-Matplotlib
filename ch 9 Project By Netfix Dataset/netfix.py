e7cd82011353442e0489e2f621bbd70f402e9393
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-24 22:17:30 +0530

    divya

--- ch 9 Project By Netfix Dataset/netfix.py
+++ ch 9 Project By Netfix Dataset/netfix.py
@@ -5,8 +5,19 @@ import pandas as pd
 
 
 # ! Step 2 : Load the data
-# from pathlib import Path
-# file_path = Path("netflix_dataset.csv")
-# df = pd.read_csv(file_path)
+
 
 df=pd.read_csv("netflix_dataset.csv")
+
+# ! Show all Col name
+# print(df.columns)
+
+df=df.dropna(subset=['type','release_year','rating','country','duration'])
+type_counts= df['type'].value_counts()
+plt.figure(figsize=(6,4))
+plt.bar(type_counts.index,type_counts.values,color=['skyblue','orange'])
+plt.title("Number of Movies VS TV shows on Netfix")
+plt.xlabel("Type")
+plt.ylabel("Count")
+plt.tight_layout()
+plt.savefig('')
