0bd33982481b406c1816f2624114df78ed1f6d06
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-05-24 19:21:21 +0530

    divya

--- ch 7 Subplots and Layout Adjustments/subplot_creation.py
+++ ch 7 Subplots and Layout Adjustments/subplot_creation.py
@@ -18,6 +18,20 @@ plt.subplot(1,2,2)   #? 1st row , 2nd Col , 2nd subplot
 
 plt.bar(x,y)
 plt.title("Bar Chart")
+# plt.show()
+
+
+# ! 2nd Way
+# ? Syntax: fig,ax=plt.subplots(nrows,ncols,figsize=[width,height])
+
+fig,ax=plt.subplots(1,2,figsize=(10,5))
+x=[1,2,3,4]
+y=[10,20,15,25]
+ax[0].plot(x,y)
+ax[0].set_title("Line Plot")
+
+ax[1].bar(x,y)
+ax[1].set_title("Bar Plot")
+
+plt.tight_layout()
 plt.show()
-
-
